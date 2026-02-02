a8540b8a50e3efebf3a9d4d00f8d0429d2dab450
jasmeeraa <jasmeerakaursood@gmail.com>
2026-02-02 14:57:29 +0530

    FullStack1

---
+++ script.js
@@ -0,0 +1,20 @@
+let heading= document.querySelector("#heading")
+console.log(heading)
+//# is selector for heading
+
+let firstElement=document.querySelector(".paragraph")
+//. is selector for paragraph
+ let para=document.querySelectorAll(".paragraph")
+console.log(para)
+
+let tag=document.querySelectorAll("p")
+console.log(tag)
+/*
+console.log("hello world");
+let heading=document.getElementById("heading")
+console.log(heading)
+let para=document.getElementsByClassName("paragraph")
+console.log(para)
+let tag=document.getElementsByTagName("p")
+console.log(tag)
+*/
